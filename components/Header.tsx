51a6039778e2b2fd20eb4dd7c901134c1c99c597
HenrikHeggland <hfh96@live.no>
2024-07-17 13:46:51 +0200

    refactor: Update Header component and add side menu functionality
    The Header component is updated to use the Material-UI AppBar and Toolbar components for a more modern and responsive design. The side menu functionality is added, allowing users to toggle the menu open and close. This change improves the visual appearance and usability of the header.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,95 @@
+"use client";
 import React from "react";
-
+import { CurrencyDisplay } from "./CurrencyDisplay";
+import {
+  AppBar,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import LoopIcon from "@mui/icons-material/Loop";
+import { useCurrency } from "./CurrencyContext";
 export const Header = () => {
+  const [open, setOpen] = React.useState(false);
+  const { resetCurrency } = useCurrency();
+
+  const toggleMenu = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  const SideMenu = (
+    <Box sx={{ width: 350 }} role="presentation" onClick={toggleMenu(false)}>
+      <List>
+        <ListItem>
+          <Typography variant="h6" component="div">
+            Settings
+          </Typography>
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem key={1} disablePadding>
+          <ListItemButton onClick={() => resetCurrency()}>
+            <ListItemIcon>
+              <LoopIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Reset stored data"} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+
   return (
-    <header className="w-full border-b text-white py-4">
-      <div className="max-w-1xl mx-auto flex justify-between items-center px-8">
-        <h2>ASCENSION CLICKER</h2>
-        {/* Add more widgets here */}
-        <div className="flex flex-row gap-5">
-          <p>CURRENCY: 0</p>
-          <p>MENU</p>
-        </div>
-      </div>
+    <header className="w-full border-b">
+      <AppBar position="static" color="default">
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit" component="div">
+            ASCENSION CLICKER
+          </Typography>
+
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: "flex" } }} gap={2}>
+            <Typography
+              variant="h6"
+              color="inherit"
+              alignContent="center"
+              component="div"
+            >
+              Currency:
+            </Typography>
+            <Typography
+              variant="h6"
+              color="inherit"
+              alignContent="center"
+              component="div"
+            >
+              <CurrencyDisplay />
+            </Typography>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={toggleMenu(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Drawer open={open} onClose={toggleMenu(false)} anchor="right">
+              {SideMenu}
+            </Drawer>
+          </Box>
+        </Toolbar>
+      </AppBar>
     </header>
   );
 };
